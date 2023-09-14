b518b7e29cacea82c43f553344def1c87b2176c9
John Tubson <johntubson@gmail.com>
2023-09-14 13:51:28 +0100

    Implement validator middlewares for auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,13 +7,16 @@ const {
 	changePassword,
 	renderResetPassword,
 } = require("../controllers/auth/authController");
+const api404 = require("../middlewares/api404");
 const protectRoute = require("../middlewares/authentication/authenticate");
-const validateUser = require("../middlewares/validation/userValidation");
+const authValidator = require("../middlewares/validation/authValidation");
 
-Router.route("/signup").post(createUserLocal);
-Router.route("/login").post(LocalLogin());
-Router.route("/resetPassword").get(renderResetPassword).post(resetPassword);
-Router.route("/forgotPassword").post(forgotPassword);
-Router.route("/changePassword").post(protectRoute, changePassword);
+Router.route("/signup").post(authValidator.localRegistration, createUserLocal);
+Router.route("/login").post(authValidator.localLogin, LocalLogin());
+Router.route("/resetPassword").post(authValidator.resetPassword, resetPassword);
+Router.route("/forgotPassword").post(authValidator.forgotPassword, forgotPassword);
+Router.route("/changePassword").post(authValidator.changePassword, protectRoute, changePassword);
+
+Router.use(api404);
 
 module.exports = Router;

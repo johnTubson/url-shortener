a7e2dc729ae1659f30b63a7431a628600db259a4
John Tubson <johntubson@gmail.com>
2023-09-14 13:39:05 +0100

    Remove deprecated mongo connection options

--- dbConnection.js
+++ dbConnection.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
 
 const mongoOptions = {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
 	autoIndex: true,
 	poolSize: 10,
-	bufferMaxEntries: 0,
 	connectTimeoutMS: 10000,
 	socketTimeoutMS: 30000,
 };
